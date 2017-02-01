59a5b0a7b7b075bfb8896ccd7313433245f3c29b
Andrew <awalwema@gmail.com>
2017-02-01 13:49:24 -0500

    Not sure what changes were made.

--- app/src/main/java/com/andrewdubs/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/andrewdubs/qrcodescanner/MainActivity.java
@@ -1,10 +1,15 @@
 package com.andrewdubs.qrcodescanner;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 
@@ -21,13 +26,18 @@ public class MainActivity extends Activity implements ZXingScannerView.ResultHan
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onClick(View v){
+
+        checkPermission();
+
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
         mScannerView.setResultHandler(this);
         mScannerView.startCamera();
+
     }
 
     @Override
@@ -36,9 +46,11 @@ public class MainActivity extends Activity implements ZXingScannerView.ResultHan
         mScannerView.stopCamera();
     }
 
+
+
     @Override
     public void handleResult(Result result) {
-        //Do what you want with said capture heare
+        //Do what you want with said capture here
         Log.w("handleResult", result.getText());
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Scan result");
@@ -48,5 +60,38 @@ public class MainActivity extends Activity implements ZXingScannerView.ResultHan
 
         //Resume scanning
         //mScannerView.resumeCameraPreview(this);
+    }
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private static final int REQUEST_CAMERA = 0;
+
+    private void checkPermission() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+
+        }
+        return;
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if(ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED)    {
+                    // Permission Granted
+                    mScannerView.startCamera();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "CAMERA Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
